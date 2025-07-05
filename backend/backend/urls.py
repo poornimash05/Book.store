259252e0478f9326c9950b21a6117396bd9e872a
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2025-07-05 20:12:54 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('api/save-token/', save_fcm_token, name='save-fcm-token'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
