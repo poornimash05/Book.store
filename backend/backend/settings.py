84f8f736e7af02be1b85230ed652c5964eac3420
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2025-07-05 19:48:54 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -163,12 +163,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 # WhiteNoise Settings
